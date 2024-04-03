1ac16f53191b74be8ba46c5560f7a0f7e91f702b
Yin Zihang <201250173@smail.nju.edu.cn>
2024-04-03 11:22:25 +0800

    init

---
+++ src/main/java/com/nju/urs_backend/UrsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nju.urs_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nju/urs_backend/UrsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nju.urs_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
